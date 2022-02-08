aa2a4374e4198747bbd2436bc331aabc1a3e0ef2
Karl Suits <karlsuits@gmail.com>
2022-02-07 21:13:34 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+/*!
+* Start Bootstrap - Shop Item v5.0.4 (https://startbootstrap.com/template/shop-item)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+
+    $("#newsButton").click(function(){
+        $("#newsModal").modal('show');
+    });
+
+});
